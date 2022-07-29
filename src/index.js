4709a706db23c3b07a1785745188902aff239cae
+ <+>
2022-07-28 22:08:27 -0400

    commit tree 0fd6f91a9a99311e381cb30d5f9913e6613d64fe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+// const init = () => {
+//  const form =  document.querySelector('form')
+//  form.addEventListener('submit', e => {e.preventDefault()
+//     e.target.children[1].value
+//     fetch(`http://localhost:3000/movies/${e.target.children[1].value}`)
+//     .then(data => console.log(data))
+//     })
+// }
+
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const input = document.querySelector('input#searchByID');
   
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
